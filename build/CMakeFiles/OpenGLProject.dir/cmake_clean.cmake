df568233200123e5f487b09945d671f25a1366e5
user8512 <dingxiaoying@mail.ustc.edu.cn>
2025-09-02 13:40:32 +0800

    initialize

---
+++ build/CMakeFiles/OpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLProject.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGLProject.dir/src/main.cpp.obj.d"
+  "OpenGLProject.exe"
+  "OpenGLProject.exe.manifest"
+  "OpenGLProject.pdb"
+  "libOpenGLProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
